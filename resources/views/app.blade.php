32fafd0c689179ae6caf9440296d24eae2c5ae23
hafiziiqbal <hafiziiqbal12345@gmail.com>
2022-05-04 10:13:19 +0700

    [PWA] configure PWA

---
+++ resources/views/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sistem Informasi Desa Jelutung</title>
+
+    <!-- Favicons -->
+    <link href="{{ asset('img/favicon.png') }}" rel="icon">
+    <link href="{{ asset('img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+
+    <!-- PWA  -->
+    <meta name="theme-color" content="#ffff" />
+    <link rel="manifest" href="{{ asset('/manifest.json') }}">
+</head>
+
+<body>
+
+    <script src="{{ asset('/sw.js') }}"></script>
+    <script>
+        if (!navigator.serviceWorker.controller) {
+            navigator.serviceWorker.register("/sw.js").then(function(reg) {
+                console.log("Service worker has been registered for scope: " + reg.scope);
+            });
+        }
+    </script>
+</body>
+
+</html>
