bfc096f954aba66615b2a98617ca8730b3c406f5
hafiziiqbal <hafiziiqbal12345@gmail.com>
2022-05-09 20:33:06 +0700

    add browsersync and certificate SSL

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,15 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.browserSync('http://jelutung-village-information-service-system.test/');
+mix.browserSync({
+    proxy: 'http://jelutung-village-information-service-system.test/',
+    https: {
+        cert: "./cert/localhost.crt",
+        key: "./cert/localhost.decrypted.key"
+    }
+});
+
 mix.js('resources/js/app.js', 'public/js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css');
