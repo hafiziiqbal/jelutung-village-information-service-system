1b3a653eb143d6e8e37c84ddeb65441ca6cf41b3
hafiziiqbal <hafiziiqbal12345@gmail.com>
2022-06-26 14:10:46 +0700

    fill permision table from seeder

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -4,16 +4,29 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 
 class PermissionSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+
     public function run()
     {
-        //
+        $permisionsArray = [
+            [
+                'name'  => 'letter service access',
+                'guard_name' => 'api'
+            ],
+            [
+                'name'  => 'manage letter and letter service',
+                'guard_name' => 'api'
+            ],
+            [
+                'name'  => 'manage website data and features',
+                'guard_name' => 'api'
+            ],
+        ];
+        foreach ($permisionsArray as  $permission) {
+            Permission::updateOrCreate($permission);
+        }
     }
 }
