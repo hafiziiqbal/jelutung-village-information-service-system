45552610ddb80f853986e4dfb1e135f597448742
hafiziiqbal <hafiziiqbal12345@gmail.com>
2022-06-27 15:58:32 +0700

    add 404 page

---
+++ resources/js/components/404NotFoundComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="error-template">
+                <h1>Oops!</h1>
+                <h2>404 Not Found</h2>
+                <div>
+                    Maaf, telah terjadi kesalahan, Halaman yang diminta tidak
+                    ditemukan!
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.error-template {
+    padding: 40px 15px;
+    text-align: center;
+}
+</style>
